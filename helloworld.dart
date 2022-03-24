93869c6dbbd4ea7271e87e894b6fa83b0831b378
Hasan Basrie <74886191+elbasrie@users.noreply.github.com>
2022-03-24 10:55:54 +0700

    Add files via upload

---
+++ helloworld.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class HelloWolrd extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Belajar Flutter"),
+      ),
+      body: const Center(
+        child: Text("Hello Wolrd"),
+      ),
+    );
+  }
+}
